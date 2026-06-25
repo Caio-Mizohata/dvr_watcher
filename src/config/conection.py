099f5de75244a7b5cc62ab7f1466c8615efbf30c
Caio-Mizohata <caiomizohata@gmail.com>
2026-06-24 23:49:48 -0300

    fix: corrige a leitura das variáveis de ambiente para minúsculas

--- src/config/conection.py
+++ src/config/conection.py
@@ -5,17 +5,17 @@ load_dotenv()
 
 def get_urls_config() -> dict[str, str]:
     # Abaixo, lê as variáveis de ambiente necessárias para construir as URLs RTSP
-    user = os.getenv("USER")
-    password = os.getenv("PASSWORD")
-    ip_address = os.getenv("IP_ADDRESS")
+    user = os.getenv("user")
+    password = os.getenv("password")
+    ip_address = os.getenv("ip_address")
 
     if not all([user, password, ip_address]):
-        raise ValueError("Configuração de câmera incompleta: USER, PASSWORD e IP_ADDRESS são obrigatórios.")
+        raise ValueError("Configuração de câmera incompleta: user, password e ip_address são obrigatórios.")
 
     # Lê os canais configurados e monta as URLs correspondentes
     urls: dict[str, str] = dict()
     for i in range(1, 3):  # Suporta até 2 canais por enquanto, podendo ser expandido facilmente
-        channel_key = f"CHANNEL_{i}"
+        channel_key = f"channel_{i}"
         channel = os.getenv(channel_key)
         if not channel:
             continue  # Se o canal não estiver configurado, pula para o próximo
